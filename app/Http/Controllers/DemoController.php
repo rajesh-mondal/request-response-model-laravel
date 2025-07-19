9eff2f5aa1df0886a683248848d748074e5d0528
Rajesh Mondal <rajesh01749@gmail.com>
2025-07-19 23:58:41 +0600

    practice simple request and response handling

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DemoController extends Controller {
+    function DemoAction(): string {
+        return "This is my first request-response";
+    }
+}
